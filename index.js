43915a8ab17e504f079e156084d128de8de89b62
Gonzalo Kerbs <gonzakerbs@gmail.com>
2024-08-15 16:51:24 -0300

    cors fix v4

--- index.js
+++ index.js
@@ -11,20 +11,11 @@ dotenv.config();
 
 conectarDB();
 
-const dominiosPermitidos = [process.env.FRONTEND_URL];
-
-const corsOptions = {
-    origin: function(origin, callback){
-        if(dominiosPermitidos.indexOf(origin) !== -1 ){
-            // el origen del request esta permitido
-            callback(null, true)
-        } else {
-            callback(new Error('No Permitido por CORS'))
-        }
-    }
-}
-
-app.use(cors( corsOptions ))
+app.use(
+    cors({
+      origin: '*',
+    }),
+  );
 
 app.use('/api/veterinarios', veterinarioRoutes);
 app.use('/api/pacientes', pacienteRoutes);

7721ef01d0243a828030cdee4beaf75c77b1d8c9
Gonzalo Kerbs <gonzakerbs@gmail.com>
2024-08-15 16:45:11 -0300

    cors fix v3

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 conectarDB();
 
-const dominiosPermitidos = '*';
+const dominiosPermitidos = [process.env.FRONTEND_URL];
 
 const corsOptions = {
     origin: function(origin, callback){
@@ -24,7 +24,7 @@ const corsOptions = {
     }
 }
 
-app.use(cors( corsOptions))
+app.use(cors( corsOptions ))
 
 app.use('/api/veterinarios', veterinarioRoutes);
 app.use('/api/pacientes', pacienteRoutes);
